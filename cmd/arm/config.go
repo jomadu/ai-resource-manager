0087bda4ef23f09565926292131506f17d45ab16
jomadu <maxdunn123@gmail.com>
2025-08-29 10:55:28 -0700

    feat: impl config

--- cmd/arm/config.go
+++ cmd/arm/config.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/jomadu/ai-rules-manager/internal/config"
 	"github.com/spf13/cobra"
 )
 
@@ -13,37 +15,35 @@ func init() {
 }
 
 var configAddCmd = &cobra.Command{
-	Use:   "add <type> <name> <url>",
+	Use:   "add <type> <name> [url]",
 	Short: "Add registry or sink configuration",
 	Long:  "Add a registry or sink to the configuration. Type must be 'registry' or 'sink'.",
-	Args:  cobra.MinimumNArgs(3),
+	Args:  cobra.MinimumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		configType := args[0]
 		name := args[1]
 
 		switch configType {
 		case "registry":
+			if len(args) < 3 {
+				return fmt.Errorf("registry requires a URL argument")
+			}
 			url := args[2]
 			registryType, _ := cmd.Flags().GetString("type")
 			if registryType == "" {
 				registryType = "git" // default
 			}
 
-			fmt.Printf("Adding registry '%s' with URL '%s' and type '%s'\n", name, url, registryType)
-			// TODO: Implement registry addition via config manager
-			return nil
+			configManager := config.NewFileManager()
+			return configManager.AddRegistry(context.Background(), name, url, registryType)
 
 		case "sink":
 			directories, _ := cmd.Flags().GetStringSlice("directories")
 			include, _ := cmd.Flags().GetStringSlice("include")
 			exclude, _ := cmd.Flags().GetStringSlice("exclude")
 
-			fmt.Printf("Adding sink '%s'\n", name)
-			fmt.Printf("  Directories: %v\n", directories)
-			fmt.Printf("  Include: %v\n", include)
-			fmt.Printf("  Exclude: %v\n", exclude)
-			// TODO: Implement sink addition via config manager
-			return nil
+			configManager := config.NewFileManager()
+			return configManager.AddSink(context.Background(), name, directories, include, exclude)
 
 		default:
 			return fmt.Errorf("invalid type '%s'. Must be 'registry' or 'sink'", configType)
@@ -59,9 +59,15 @@ var configRemoveCmd = &cobra.Command{
 		configType := args[0]
 		name := args[1]
 
-		fmt.Printf("Removing %s '%s'\n", configType, name)
-		// TODO: Implement removal via config manager
-		return nil
+		configManager := config.NewFileManager()
+		switch configType {
+		case "registry":
+			return configManager.RemoveRegistry(context.Background(), name)
+		case "sink":
+			return configManager.RemoveSink(context.Background(), name)
+		default:
+			return fmt.Errorf("invalid type '%s'. Must be 'registry' or 'sink'", configType)
+		}
 	},
 }
 
@@ -69,8 +75,28 @@ var configListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List configuration",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Configuration:")
-		// TODO: Implement listing via config manager
+		configManager := config.NewFileManager()
+		registries, err := configManager.GetRegistries(context.Background())
+		if err != nil {
+			return err
+		}
+		sinks, err := configManager.GetSinks(context.Background())
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Registries:")
+		for name, reg := range registries {
+			fmt.Printf("  %s: %s (%s)\n", name, reg.URL, reg.Type)
+		}
+
+		fmt.Println("Sinks:")
+		for name, sink := range sinks {
+			fmt.Printf("  %s:\n", name)
+			fmt.Printf("    directories: %v\n", sink.Directories)
+			fmt.Printf("    include: %v\n", sink.Include)
+			fmt.Printf("    exclude: %v\n", sink.Exclude)
+		}
 		return nil
 	},
 }
