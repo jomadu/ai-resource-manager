1650c9ba507e8dcaf9aa13086c55f3ad59c6ad2a
jomadu <maxdunn123@gmail.com>
2025-09-01 21:46:06 -0700

    fix: cache locking

--- internal/cache/registry_lock.go
+++ internal/cache/registry_lock.go
@@ -9,45 +9,30 @@ import (
 
 var registryLocks sync.Map // map[string]*sync.Mutex
 
-type RegistryLock struct {
-	file *os.File
-	mu   *sync.Mutex
-}
-
-func AcquireRegistryLock(registryDir string) (*RegistryLock, error) {
+func WithRegistryLock(registryKey string, fn func() error) error {
 	// Get or create mutex for this registry
-	mutex, _ := registryLocks.LoadOrStore(registryDir, &sync.Mutex{})
+	mutex, _ := registryLocks.LoadOrStore(registryKey, &sync.Mutex{})
 	mu := mutex.(*sync.Mutex)
 	mu.Lock()
+	defer mu.Unlock()
 
-	// Create file lock
-	lockFile := filepath.Join(registryDir, ".lock")
-	if err := os.MkdirAll(registryDir, 0o755); err != nil {
-		mu.Unlock()
-		return nil, err
+	// Create file lock in dedicated .locks subdirectory
+	cacheDir := GetCacheDir()
+	locksDir := filepath.Join(cacheDir, ".locks")
+	lockFile := filepath.Join(locksDir, registryKey+".lock")
+	if err := os.MkdirAll(locksDir, 0o755); err != nil {
+		return err
 	}
 
 	file, err := os.OpenFile(lockFile, os.O_CREATE|os.O_WRONLY, 0o644)
 	if err != nil {
-		mu.Unlock()
-		return nil, err
+		return err
 	}
+	defer func() { _ = file.Close() }()
 
 	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
-		_ = file.Close()
-		mu.Unlock()
-		return nil, err
+		return err
 	}
 
-	return &RegistryLock{file: file, mu: mu}, nil
-}
-
-func (rl *RegistryLock) Release() error {
-	defer rl.mu.Unlock()
-	return rl.file.Close()
-}
-
-func (rl *RegistryLock) ReleaseIgnoreError() {
-	defer rl.mu.Unlock()
-	_ = rl.file.Close()
+	return fn()
 }
