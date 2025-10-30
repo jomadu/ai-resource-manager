db7d24034996319628dc0a683165fb6933ea3a43
jomadu <maxdunn123@gmail.com>
2025-10-29 20:56:17 -0700

    test: consistent order

--- internal/resource/compiler.go
+++ internal/resource/compiler.go
@@ -1,6 +1,8 @@
 package resource
 
 import (
+	"sort"
+
 	"github.com/jomadu/ai-rules-manager/internal/types"
 )
 
@@ -49,7 +51,16 @@ func NewCompiler(target CompileTarget) (Compiler, error) {
 // CompileRuleset compiles a ruleset to the target format
 func (c *DefaultCompiler) CompileRuleset(namespace string, ruleset *Ruleset) ([]*types.File, error) {
 	var compiledFiles []*types.File
-	for ruleID, rule := range ruleset.Spec.Rules {
+
+	// Sort rule IDs for consistent ordering
+	ruleIDs := make([]string, 0, len(ruleset.Spec.Rules))
+	for ruleID := range ruleset.Spec.Rules {
+		ruleIDs = append(ruleIDs, ruleID)
+	}
+	sort.Strings(ruleIDs)
+
+	for _, ruleID := range ruleIDs {
+		rule := ruleset.Spec.Rules[ruleID]
 		filename := c.ruleFilenameGen.GenerateFilename(ruleset.Metadata.ID, ruleID)
 		content := c.ruleGen.GenerateRule(namespace, ruleset, ruleID, &rule)
 		compiledFiles = append(compiledFiles, &types.File{
@@ -65,7 +76,16 @@ func (c *DefaultCompiler) CompileRuleset(namespace string, ruleset *Ruleset) ([]
 // CompilePromptset compiles a promptset to the target format
 func (c *DefaultCompiler) CompilePromptset(namespace string, promptset *Promptset) ([]*types.File, error) {
 	var compiledFiles []*types.File
-	for promptID, prompt := range promptset.Spec.Prompts {
+
+	// Sort prompt IDs for consistent ordering
+	promptIDs := make([]string, 0, len(promptset.Spec.Prompts))
+	for promptID := range promptset.Spec.Prompts {
+		promptIDs = append(promptIDs, promptID)
+	}
+	sort.Strings(promptIDs)
+
+	for _, promptID := range promptIDs {
+		prompt := promptset.Spec.Prompts[promptID]
 		filename := c.promptFilenameGen.GenerateFilename(promptset.Metadata.ID, promptID)
 		content := c.promptGen.GeneratePrompt(namespace, promptset, promptID, &prompt)
 		compiledFiles = append(compiledFiles, &types.File{
